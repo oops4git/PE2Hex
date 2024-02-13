1ca96d0e4978168db5afd63f77320f6d58339e54
Steven <oops4git@sina.com>
2024-02-13 20:18:54 +0800

    Update PE2Hex.c
    Edit file

--- PE2Hex.c
+++ PE2Hex.c
@@ -11,33 +11,33 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    const char* exePath = argv[1]; // 从命令行参数接收EXE文件路径
+    const char* exePath = argv[1]; 
 
-    // 提取文件名（不包含路径）
+
     const char* filename = strrchr(exePath, '\\');
-    if (!filename) { // 处理不包含路径分隔符的情况
+    if (!filename) { 
         filename = exePath;
     }
     else {
-        filename++; // 移过路径分隔符
+        filename++; 
     }
 
-    char outputPath[260]; // 存储输出路径，假设路径长度不超过259字符
-    snprintf(outputPath, sizeof(outputPath), "pe_%s.txt", filename); // 构建输出文件路径
+    char outputPath[260]; 
+    snprintf(outputPath, sizeof(outputPath), "pe_%s.txt", filename); 
 
-    // 以二进制读取模式打开EXE文件
+
     fexe = fopen(exePath, "rb");
     if (!fexe) {
         perror("Failed to open executable");
         return 1;
     }
 
-    // 移动到文件末尾以确定文件大小
+
     fseek(fexe, 0, SEEK_END);
     long fileSize = ftell(fexe);
-    rewind(fexe); // 回到文件开头
+    rewind(fexe); 
 
-    // 分配内存以存储文件内容
+
     unsigned char* buf = (unsigned char*)malloc(fileSize);
     if (!buf) {
         perror("Memory allocation failed");
@@ -45,11 +45,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 读取文件内容到缓冲区
-    fread(buf, 1, fileSize, fexe);
-    fclose(fexe); // 关闭EXE文件
 
-    // 打开输出文件
+    fread(buf, 1, fileSize, fexe);
+    fclose(fexe); 
+
+
     fout = fopen(outputPath, "w");
     if (!fout) {
         perror("Failed to open output file");
@@ -59,26 +59,26 @@ int main(int argc, char* argv[]) {
 
     
     fprintf(fout, "unsigned char pBuffer[] = \n");
-    // 将缓冲区内容输出到文本文件，每个字节前加\x，每16个字节换行，行首行尾加双引号
+
     for (long i = 0; i < fileSize; i++) {
-        if (i % 16 == 0) { // 每16个字节或文件开始时
-            if (i != 0) { // 如果不是文件的第一个字节，先结束前一行
+        if (i % 16 == 0) { 
+            if (i != 0) { 
                 fprintf(fout, "\"\n");
             }
-            fprintf(fout, "\""); // 行开始加双引号
+            fprintf(fout, "\""); 
         }
-        fprintf(fout, "\\x%02x", buf[i]); // 以十六进制形式输出，每个字节前加\x
-        if (i == fileSize - 1) { // 文件结束时
-            fprintf(fout, "\""); // 行尾加双引号
+        fprintf(fout, "\\x%02x", buf[i]); 
+        if (i == fileSize - 1) { 
+            fprintf(fout, "\""); 
         }
     }
 
-    if (fileSize % 16 != 0) { // 如果最后一行不足16个字节，也需要在行尾加上双引号
+    if (fileSize % 16 != 0) { 
         fprintf(fout, "\"");
     }
-    fprintf(fout, ";\n"); // 文件输出完毕后换行
+    fprintf(fout, ";\n"); 
 
-    // 清理资源
+
     fclose(fout);
     free(buf);
 
